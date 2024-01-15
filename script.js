6a5d8819cd688e06881eadcda4d669effce3b690
Glitch (glitch-hello-website) <none>
2024-01-15 14:07:21 +0000

    😋🕋 Checkpoint ./script.js:15540831/1750 ./index.html:15540831/296

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-const getDefaultSession = solidAuthn.getDefaultSession;
-const Parser = n3.Parser;
+import { getDefaultSession } from 'https://cdn.skypack.dev/pin/@inrupt/solid-client-authn-browser@v2.0.0-3Py1cpWfOrpxuIlTz5M2/dist=es2019,mode=imports/optimized/@inrupt/solid-client-authn-browser.js'
+import { Parser } from 'https://cdn.skypack.dev/pin/n3@v1.12.0-JyCuQEtqH88WU0Kn0PZm/mode=imports,min/optimized/n3.js'
 
-document.getElementById('moviePicker').innerHTML = "Loaded"
+const simply = window.simply
+
+const app = simply.app({
+  commands: {
+    'loadMovies': (el, value) => {
+      alert('jay')
+    }
+  }
+})
+
